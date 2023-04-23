ac19443d3d4f59eaa5c07e58704ea46969421c74
Derek Prieur <DPrieur123@gmail.com>
2023-04-23 15:06:45 -0400

    tab info

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import { Header } from '../components'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <div className='min-h-screen bg-[#fefefe]'>
+      <Head>
+        <title>Robo Pulse</title>
+        <meta name="description" content="A description of your website" />
+        <link rel="icon" href="/robo.png" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </div>
